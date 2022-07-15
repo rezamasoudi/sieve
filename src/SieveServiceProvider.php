3d65104549e3d966ec044ded9c4a4e75012bf7e1
Masoudi <dev.masoudi@gmail.com>
2022-07-15 09:47:36 +0430

    Remove unused register function

--- src/SieveServiceProvider.php
+++ src/SieveServiceProvider.php
@@ -7,11 +7,6 @@ use Masoudi\Sieve\Console\CreateFilter;
 
 class SieveServiceProvider extends ServiceProvider
 {
-    public function register()
-    {
-        //
-    }
-
     public function boot()
     {
 
